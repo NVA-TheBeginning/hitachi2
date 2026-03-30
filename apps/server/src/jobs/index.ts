a4272510450a153e148828b89eee7bc0c976de93
Antony David <antonydavid945@gmail.com>
2026-03-30 16:37:18 +0200

    feat: setup pgboss skeleton

---
+++ apps/server/src/jobs/index.ts
@@ -0,0 +1,9 @@
+import type { PgBoss } from "pg-boss";
+
+export async function registerWorkers(boss: PgBoss) {
+  await boss.work<{ to: string; subject: string }>("send-email", async (jobs) => {
+    for (const job of jobs) {
+      console.log("Processing send-email job", job.id, job.data);
+    }
+  });
+}
